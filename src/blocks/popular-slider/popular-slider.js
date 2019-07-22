5224e69b8cc8ad627a79b6a28d17c4692a222482
des-yogi <nuevo-design@mail.ru>
2019-07-22 19:14:03 +0300

    catalog in progress

---
+++ src/blocks/popular-slider/popular-slider.js
@@ -0,0 +1,33 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  var populatSlider = new Swiper('.popular-slider__container', {
+      spaceBetween: 20,
+      grabCursor: true,
+      breakpointsInverse: true,
+      lazy: true,
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1
+        },
+        600: {
+          slidesPerView: 2
+        },
+        992: {
+          slidesPerView: 3
+        },
+        1280: {
+          slidesPerView: 4
+        },
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    });
+});
+
