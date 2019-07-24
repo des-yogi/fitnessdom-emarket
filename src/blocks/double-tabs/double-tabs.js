41816420c388d96381a8131900b9b126eac6a6aa
des-yogi <nuevo-design@mail.ru>
2019-07-24 18:29:38 +0300

    index-markup in progress

--- src/blocks/double-tabs/double-tabs.js
+++ src/blocks/double-tabs/double-tabs.js
@@ -1,8 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  var newsSlider = new Swiper('#hits-slider', {
+  var newsSlider = new Swiper('#news-slider', {
       slidesPerView: 1,
       spaceBetween: 20,
+      //loop: true,
+      observer: true,
+      observeParents: true,
+      observeSlideChildren: true,
       // autoplay: {
       //   delay: 3000,
       // },
@@ -15,20 +19,31 @@ document.addEventListener('DOMContentLoaded', function () {
         600: {
           slidesPerView: 2
         },
-        768: {
+        900: {
           slidesPerView: 3
         },
-        992: {
+        1280: {
           slidesPerView: 4
         }
-      }
-      // ,
-      // lazy: true
+      },
+      pagination: {
+        el: '.double-tabs__pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.double-tabs__next-hits',
+        prevEl: '.double-tabs__prev-hits',
+      },
+      lazy: true
   });
 
-  var hitsSlider = new Swiper('#news-slider', {
+  var hitsSlider = new Swiper('#hits-slider', {
       slidesPerView: 1,
       spaceBetween: 20,
+      //loop: true,
+      observer: true,
+      observeParents: true,
+      observeSlideChildren: true,
       breakpointsInverse: true,
       breakpoints: {
         // when window width is >= 320px
@@ -38,38 +53,59 @@ document.addEventListener('DOMContentLoaded', function () {
         600: {
           slidesPerView: 2
         },
-        768: {
+        900: {
           slidesPerView: 3
         },
-        992: {
+        1280: {
           slidesPerView: 4
         }
-      }
-      // ,
-      // lazy: true
+      },
+      pagination: {
+        el: '.double-tabs__pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.double-tabs__next-hits',
+        prevEl: '.double-tabs__prev-hits',
+      },
+      lazy: true
   });
 
-  var doubleTabs = document.querySelector('.double-tabs');
-  var navLinks = document.querySelectorAll('.nav-link');
-  var tabHits = document.querySelector('#hits');
-  var tabNews = document.querySelector('#news');
+  // var doubleTabs = document.querySelector('.double-tabs');
+  // var navLinks = document.querySelectorAll('.nav-link');
+  // var tabHits = document.querySelector('#hits');
+  // var tabNews = document.querySelector('#news');
+  // var nextBtn = document.querySelector('.double-tabs__next-hits');
+  // var prevBtn = document.querySelector('.double-tabs__prev-hits');
 
-  if (!doubleTabs) { return; }
 
-  const navLinkClickHandler = function (e) {
-    newsSlider.update();
-    hitsSlider.update();
-    /*if ( e.target.id === 'news-tab' ) {
-      newsSlider.update();
+  // if (!doubleTabs) { return; }
+
+/*  const navLinkClickHandler = function (e) {
+
+    if (e.target.id === 'hit-tab') {
+
+      nextBtn.classList.remove('double-tabs__next-news');
+      prevBtn.classList.remove('double-tabs__prev-news');
+      nextBtn.classList.add('double-tabs__next-hits');
+      prevBtn.classList.add('double-tabs__prev-hits');
     }
-      hitsSlider.update();*/
-  };
+    else {
+      nextBtn.classList.remove('double-tabs__next-hits');
+      prevBtn.classList.remove('double-tabs__prev-hits');
+      nextBtn.classList.add('double-tabs__next-news');
+      prevBtn.classList.add('double-tabs__prev-news');
+    }
 
-  /*tabHits.addEventListener('change', navLinkClickHandler);
-  tabNews.addEventListener('change', navLinkClickHandler);*/
-  Array.prototype.forEach.call(navLinks, function (link) {
+    setTimeout( function() {
+
+    }, 400);
+
+  };*/
+
+  /*Array.prototype.forEach.call(navLinks, function (link) {
     link.addEventListener('click', navLinkClickHandler);
-  });
+  });*/
 
 });
 
--- src/blocks/special-offer-slider/special-offer-slider.js
+++ src/blocks/special-offer-slider/special-offer-slider.js
@@ -1,4 +1,35 @@
-// document.addEventListener('DOMContentLoaded', function(){});
-// (function(){
-// код
-// }());
+document.addEventListener('DOMContentLoaded', function () {
+
+  var specOfferSlider = new Swiper('.special-offer-slider__container', {
+      slidesPerView: 2,
+      slidesPerColumn: 2,
+      spaceBetween: 20,
+      //loop: true,
+      observer: true,
+      observeParents: true,
+      observeSlideChildren: true,
+      // autoplay: {
+      //   delay: 3000,
+      // },
+      breakpointsInverse: true,
+      breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1
+        },
+        1280: {
+          slidesPerView: 2
+        }
+      },
+      pagination: {
+        el: '.special-offer-slider__pagination',
+        type: 'fraction',
+      },
+      navigation: {
+        nextEl: '.special-offer-slider__next',
+        prevEl: '.special-offer-slider__prev',
+      },
+      lazy: true
+  });
+
+});
