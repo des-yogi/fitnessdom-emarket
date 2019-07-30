e44cb05c9adefb8dd5d96105d8acdf3f95e1a820
des-yogi <nuevo-design@mail.ru>
2019-07-30 11:09:17 +0300

    rebuild

--- src/blocks/mobile-nav/mobile-nav.js
+++ src/blocks/mobile-nav/mobile-nav.js
@@ -1,13 +1,30 @@
 document.addEventListener(
   "DOMContentLoaded", () => {
-    new Mmenu( '#mobile-nav', {
+    const mmenu = new Mmenu( '#mobile-nav', {
         // options
         'extensions': [
-            'pagedim-black'
+            'theme-white',
+            'pagedim-black',
+            'border-none'
          ],
+         'navbar': {
+            title: 'fitness<span>DOM</span> <span id="mobile-nav-close"></span>' //<a href="#" id="close-btn">X</a>
+         },
          'navbars': [
+            /*{
+
+              'content': [
+                  'breadcrumbs',
+                  'close'
+               ]
+            },*/
             {
-              'position': 'top'
+               'position': 'bottom',
+               'content': [
+                  "<a class='mobile-nav__social mobile-nav__social--fb' href='#' title='Facebook'><span></span></a>",
+                  "<a class='mobile-nav__social mobile-nav__social--insta' href='#' title='Instagram'><span></span></a>",
+                  "<a class='mobile-nav__social mobile-nav__social--youtube' href='#' title='Youtube'><span></span></a>"
+               ]
             }
          ]
     }, {
@@ -21,6 +38,15 @@ document.addEventListener(
           }
         }
     });
+
+    const api = mmenu.API;
+
+    document.querySelector( "#mobile-nav-close" )
+        .addEventListener(
+            "click", ( evnt ) => {
+                evnt.preventDefault();
+                api.close();
+        }
+    );
   }
 );
-
